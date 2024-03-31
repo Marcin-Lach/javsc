ac9819bf657de4703b273e3f6c68529e654857ba
Marcin Lach <marcin.lach.90@gmail.com>
2024-03-31 23:30:33 +0200

    Adding dash action (small refactor)

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -14,29 +14,31 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-	
-func set_velocity():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-
-	if _current_speed_multiplier == 1.0:
-		if Input.is_action_just_pressed("dash"):
-			_current_speed_multiplier = _dash_speed_multiplier
-			
-			var speed_multiplier_tween = (create_tween()
-					.set_pause_mode(Tween.TWEEN_PAUSE_STOP)
-					.set_ease(Tween.EASE_IN_OUT)
-					.set_trans(Tween.TRANS_BACK)) # this will make player exceed the _dash_speed_multiplier briefly, but then _current_speed_multiplier will fall below 1.0 for a short period
-			speed_multiplier_tween.tween_property(self, "_current_speed_multiplier", 1.0, 0.5)
-			
-	print(str("_current_speed_multiplier: ", _current_speed_multiplier))
-	linear_velocity = input_direction * _speed * _current_speed_multiplier
 
 
 func _physics_process(delta):
-	set_velocity()	
+	_set_velocity()	
 
 
 func _on_body_entered(body):
 	if body.is_in_group("enemies"):
 		get_tree().reload_current_scene()
+
+
+func _set_velocity():
+	var input_direction = Input.get_vector("left", "right", "up", "down") as Vector2
+	_handle_dash(input_direction)
+	linear_velocity = input_direction * _speed * _current_speed_multiplier
+
+
+func _handle_dash(current_input_direction : Vector2):
+	if current_input_direction.is_zero_approx() == false:
+		if Input.is_action_just_pressed("dash"):
+			if _current_speed_multiplier == 1.0:
+				_current_speed_multiplier = _dash_speed_multiplier
+				
+				var speed_multiplier_tween = (create_tween()
+						.set_pause_mode(Tween.TWEEN_PAUSE_STOP)
+						.set_ease(Tween.EASE_IN_OUT)
+						.set_trans(Tween.TRANS_BACK)) # this will make player exceed the _dash_speed_multiplier briefly, but then _current_speed_multiplier will fall below 1.0 for a short period
+				speed_multiplier_tween.tween_property(self, "_current_speed_multiplier", 1.0, 0.5)
